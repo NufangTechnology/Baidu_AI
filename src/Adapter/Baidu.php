d684b44ca69fea0d9a2ac06eb949d0c3ae7c62f2
南山吴彦祖 <anoxia@engineer.com>
2019-01-21 22:43:13 +0800

    优化代码

--- src/Adapter/Baidu.php
+++ src/Adapter/Baidu.php
@@ -3,7 +3,7 @@
 namespace NFTech\ASR\Adapter;
 
 use NFTech\ASR\AsrInterface;
-use NFTech\ASR\Exception;
+use Swoole\Coroutine;
 use Swoole\Coroutine\Http\Client;
 
 class Baidu implements AsrInterface
@@ -56,7 +56,6 @@ class Baidu implements AsrInterface
      * 语音识别
      *
      * @param $filePath string 语音文件本地路径,优先使用此项
-     * @param $userID string 用户唯一标识
      * @param $format string 语音文件格式 ['pcm', 'wav', 'opus', 'speex', 'amr']
      * @param $rate integer 采样率 [8000, 16000]
      * @param int $dev_pid string 模型id 1537:普通话(纯中文识别)  1536：    普通话(支持简单的英文识别) 1737：英语 1637:粤语  1837：四川话
@@ -76,8 +75,7 @@ class Baidu implements AsrInterface
         }
 
         // 异步读取文件
-//        $content = Coroutine::readFile($filePath);
-        $content = file_get_contents($filePath);
+        $content = Coroutine::readFile($filePath);
         if ($content == false) {
             throw new BaiduException('音频文件[' . $filePath . ']读取失败');
         }
@@ -105,7 +103,7 @@ class Baidu implements AsrInterface
      *
      * @param array $options
      * @return mixed
-     * @throws Exception
+     * @throws BaiduException
      */
     protected function request(array $options)
     {
@@ -135,11 +133,11 @@ class Baidu implements AsrInterface
 
         // 获取结果失败
         if (!is_array($result)) {
-            throw new Exception('获取音频转换结果失败：' . $client->body);
+            throw new BaiduException('获取音频转换结果失败：' . $client->statusCode . ' | ' . $client->body . ' | ' . $client->errMsg, $client->errCode);
         }
         // 转换出错
         if ($result['err_no'] > 0) {
-            throw new Exception($result['err_msg'], $result['err_no']);
+            throw new BaiduException($result['err_msg'], $result['err_no']);
         }
 
         return $result['result'];
@@ -149,7 +147,7 @@ class Baidu implements AsrInterface
      * 账户身份认证
      *
      * @return array
-     * @throws Exception
+     * @throws BaiduException
      */
     private function identityAuth()
     {
@@ -172,11 +170,12 @@ class Baidu implements AsrInterface
             // 授权信息
             $result = json_decode($client->body, true);
 
-            // 认证失败
+            // 身份认证失败
             if (isset($result['error'])) {
-                throw new Exception('身份认证失败：' . $result['error_description']);
+                throw new BaiduException('身份认证失败：' . $result['error_description']);
             }
-            //
+
+            // 云用户
             if (stripos('brain_all_scope', $result['scope'])) {
                 $this->isCloudUser = true;
             }
@@ -184,73 +183,10 @@ class Baidu implements AsrInterface
             // 用户授权信息
             $this->authInfo = $result;
             // 计算过期时间（每 25 天重新获取一次）
-            $this->freshAuthTimestamp = $time + 86400 * 25;
+            $this->freshAuthTimestamp = $time + (86400 * 25);
         }
 
         return $this->authInfo;
-    }
-
-    /**
-     * 处理请求参数
-     * @param string $url
-     * @param array $params
-     * @param array $data
-     * @param array $headers
-     */
-    protected function processRequest(string $url, array &$params, array &$data, $headers)
-    {
-        $token        = isset($params['access_token']) ? $params['access_token'] : '';
-        $data['cuid'] = md5($token);
-
-        if ($url === $this->asrUrl) {
-            $data['token'] = $token;
-            $data          = json_encode($data);
-        } else {
-            $data['tok'] = $token;
-        }
-
-        unset($params['access_token']);
-    }
-
-    /**
-     * @param  string $method HTTP method
-     * @param  string $url
-     * @param  array $param 参数
-     * @return array
-     */
-    private function getAuthHeaders($method, $url, $params = array(), $headers = array())
-    {
-
-        //不是云的老用户则不用在header中签名 认证
-        if ($this->isCloudUser === false) {
-            return $headers;
-        }
-
-        $obj = parse_url($url);
-        if (!empty($obj['query'])) {
-            foreach (explode('&', $obj['query']) as $kv) {
-                if (!empty($kv)) {
-                    list($k, $v) = explode('=', $kv, 2);
-                    $params[$k] = $v;
-                }
-            }
-        }
-
-        //UTC 时间戳
-        $timestamp             = gmdate('Y-m-d\TH:i:s\Z');
-        $headers['Host']       = isset($obj['port']) ? sprintf('%s:%s', $obj['host'], $obj['port']) : $obj['host'];
-        $headers['x-bce-date'] = $timestamp;
-
-        //签名
-        $headers['authorization'] = AipSampleSigner::sign(array(
-            'ak' => $this->apiKey,
-            'sk' => $this->secretKey,
-        ), $method, $obj['path'], $headers, $params, array(
-            'timestamp'     => $timestamp,
-            'headersToSign' => array_keys($headers),
-        ));
-
-        return $headers;
     }
 }
 
